9c8ab4f5f3db49c077fb94a4248b0b8fde5a2517
faisalsaid <falsdesain@gmail.com>
2023-08-02 20:27:44 +0900

    reset all

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-// const express = require('express')
-// const app = express()
-// app.all('/', (req, res) => {
-//     console.log("Just got a request!")
-//     res.send('Upadate')
-// })
-
-
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./server/db.json');
-const middlewares = jsonServer.defaults({
-  static: './build',
-});
-
-server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1',
-  }),
-);
-
-server.use(router);
-
-
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Upadate')
+})
 app.listen(process.env.PORT || 3000)
