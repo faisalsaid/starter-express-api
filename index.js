271e9b2b9f25168a744d9638d8ae5c8180ea122d
faisalsaid <falsdesain@gmail.com>
2023-08-02 20:22:07 +0900

    add json server

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Upadate')
-})
+// const express = require('express')
+// const app = express()
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Upadate')
+// })
+
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./server/db.json');
+const middlewares = jsonServer.defaults({
+  static: './build',
+});
+
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  }),
+);
+
+server.use(router);
+
+
 app.listen(process.env.PORT || 3000)
